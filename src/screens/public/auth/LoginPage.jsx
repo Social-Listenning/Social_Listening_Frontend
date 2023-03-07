6f81cfb6f997e1ced78bcfa4a9e5dbfa78b07dd1
vunh <59404796+vu2872001@users.noreply.github.com>
2023-03-07 13:46:46 +0700

    add profile page

--- src/screens/public/auth/LoginPage.jsx
+++ src/screens/public/auth/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Checkbox, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { customHistory } from '../../../routes/CustomRouter';
